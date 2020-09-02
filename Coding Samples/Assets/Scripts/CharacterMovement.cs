dbebeea488493c9333fbd4e093d90bc8da7d5797
Kaden(LenLap) <10836246@my.uvu.edu>
2020-09-01 23:21:25 -0600

    Worked on CharacterMovement

---
+++ Coding Samples/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterMovement : MonoBehaviour
+{
+	private CharacterController myController;
+	private Vector3 myV3Movement;
+	public float mySpeed = 1f;
+	public float myGravity = -9.81f;
+	public bool canJump;
+	public int maxJumpCount;
+
+	void Start()
+	{
+	    myController = GetComponent<CharacterController>();
+	}
+
+
+	void Update()
+	{
+	    if(Input.GetKeyDown(KeyCode.A))
+		{
+			//myV3Movement.x *= -Input.GetAxis("Horizontal") * mySpeed;
+			myV3Movement.x += -mySpeed;
+		}      
+
+		if(Input.GetKeyDown(KeyCode.D))
+		{
+			//myV3Movement.x *= Input.GetAxis("Horizontal") * mySpeed;
+			myV3Movement.x += mySpeed;
+		}
+
+		if(!myController.isGrounded)
+		{
+			myV3Movement.y += myGravity;
+		}
+		else
+		{
+			myV3Movement.y = 0;
+		}
+
+		myV3Movement = transform.TransformDirection(myV3Movement);
+		myController.Move(myV3Movement * Time.deltaTime);
+	}
+}
+
+
