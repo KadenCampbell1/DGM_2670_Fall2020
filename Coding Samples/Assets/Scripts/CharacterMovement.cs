8004ac055dede7d7f2a5c13bd153be905bac0a42
Kaden(LenLap) <10836246@my.uvu.edu>
2020-09-06 23:36:28 -0600

    Moved camera to 30 degrees; Updated walk on CharacterMovement

--- Coding Samples/Assets/Scripts/CharacterMovement.cs
+++ Coding Samples/Assets/Scripts/CharacterMovement.cs
@@ -8,8 +8,9 @@ public class CharacterMovement : MonoBehaviour
 	private int jumpCount;
 	private float yAxisVar;
 
-	public float mySpeed = 5f, myRunSpeed = 10f, myRotateSpeed = 150f, myJumpForce = 5f, myGravity = -9.81f;
+	public float mySpeed = 5f, myRunSpeed = 10f, myCarSpeed = 20f, myRotateSpeed = 150f, myRotateBoost = 100f, myJumpForce = 5f, myGravity = -9.81f;
 	public int maxJumpCount = 2;
+	public bool drivingCar = false;
 	
 	void Start()
 	{
@@ -19,24 +20,6 @@ public class CharacterMovement : MonoBehaviour
 
 	void Update()
 	{
-		if (Input.GetButtonDown("Fire3"))
-		{
-			mySpeed += myRunSpeed;
-			myRotateSpeed += myRunSpeed * 10;
-		}
-		else if (Input.GetButtonUp("Fire3"))
-		{
-			mySpeed -= myRunSpeed;
-			myRotateSpeed -= myRunSpeed * 10;
-		}
-		
-		//perhaps put horizontalInput in Z in Set function for character to move unlike a car. Use this code for controlling Chrysalises.
-		var verticalInput = Input.GetAxis("Vertical") * mySpeed;
-		v3Movement.Set(verticalInput, yAxisVar, 0);
-
-		var horizontalInput = Input.GetAxis("Horizontal") * Time.deltaTime * myRotateSpeed;
-		transform.Rotate(0, horizontalInput, 0);
-
 		yAxisVar += myGravity * Time.deltaTime;
 
 		if (myController.isGrounded && v3Movement.y < 0)
@@ -45,12 +28,45 @@ public class CharacterMovement : MonoBehaviour
 			jumpCount = 0;
 		}
 
-		if (Input.GetButtonDown("Jump") && jumpCount < maxJumpCount)
+		if (!drivingCar)
 		{
-			yAxisVar = myJumpForce;
-			jumpCount++;
+			if (Input.GetButtonDown("Fire3"))
+			{
+				mySpeed += myRunSpeed;
+			}
+			else if (Input.GetButtonUp("Fire3"))
+			{
+				mySpeed -= myRunSpeed;
+			}
+			
+			if (Input.GetButtonDown("Jump") && jumpCount < maxJumpCount)
+			{
+				yAxisVar = myJumpForce;
+				jumpCount++;
+			}
+			
+			var verticalInput = Input.GetAxis("Vertical") * mySpeed;
+			var horizontalInput = Input.GetAxis("Horizontal") * mySpeed;
+			v3Movement.Set(horizontalInput, yAxisVar, verticalInput);
 		}
-
+		if (drivingCar)
+		{
+			var verticalInput = Input.GetAxis("Vertical") * myCarSpeed;
+			v3Movement.Set(verticalInput, yAxisVar, 0);
+			
+			if (Input.GetButtonDown("Fire3"))
+			{
+				myRotateSpeed += myRotateBoost;
+			}
+			else if (Input.GetButtonUp("Fire3"))
+			{
+				myRotateSpeed -= myRotateBoost;
+			}
+			
+			var horizontalInput = Input.GetAxis("Horizontal") * Time.deltaTime * myRotateSpeed;
+			transform.Rotate(0, horizontalInput, 0);
+		}
+		
 		v3Movement = transform.TransformDirection(v3Movement);
 		myController.Move(v3Movement * Time.deltaTime);
 	}
