50f37d7fe8dffcb0c23e593ff951afb1ed35e8ad
Kaden(LenLap) <10836246@my.uvu.edu>
2020-09-12 23:18:07 -0600

    Worked on Death in PlayerHealth script

--- Coding Samples/Assets/Scripts/PlayerHealth.cs
+++ Coding Samples/Assets/Scripts/PlayerHealth.cs
@@ -5,26 +5,52 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public IntData health, respawnHealth;
+    public IntData health, respawnHealth, lifeTotal;
+    private int gameOver = 0;
     public Vector3Data currentSpawn;
     public int waitTime;
+    private MeshRenderer meshRenderer;
+    private CharacterController characterController;
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    // private IEnumerator DeathEvent()
+    //     {
+    //         meshRenderer.enabled = false;
+    //         characterController.enabled = false;
+    //         yield return new WaitForSeconds(waitTime);
+    //         health.myValue = respawnHealth.myValue;
+    //         gameObject.transform.position = currentSpawn.myValue;
+    //         meshRenderer.enabled = true;
+    //         characterController.enabled = true;
+    //     }
+
+    private void DeathEvent()
+    {
+        if (lifeTotal.myValue > gameOver)
+        {
+            gameObject.transform.position = currentSpawn.myValue;
+            health.myValue = respawnHealth.myValue;
+            lifeTotal.myValue--;
+        }
+        else
+        {
+            gameObject.transform.position = currentSpawn.myValue;
+        }
+        
+    }
 
     private void Update()
     {
         if (health.myValue <= 0)
         {
-            //invoke the EVENT
+            DeathEvent();
         }
     }
 
-    private IEnumerator DeathEvent()
-    {
-        GetComponent<MeshRenderer>().enabled = false;
-        GetComponent<CharacterController>().enabled = false;
-        yield return new WaitForSeconds(waitTime);
-        health.myValue = respawnHealth.myValue;
-        gameObject.transform.position = currentSpawn.myValue;
-        GetComponent<MeshRenderer>().enabled = true;
-        GetComponent<CharacterController>().enabled = true;
-    }
+    
 }
