8d24bc82bc9330b75d5404a4cef806fef62d0855
Kaden(LenLap) <10836246@my.uvu.edu>
2020-09-14 23:58:32 -0600

    Began instancer

---
+++ Coding Samples/Assets/Scripts/Instancer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instancer : MonoBehaviour
+{
+    public GameObject prefab;
+
+    private void Start()
+    {
+        var location = transform.position;
+        var rotationDirection = new Vector3(0, 45, 0);
+        Instantiate(prefab, location, Quaternion.Euler(rotationDirection));
+    }
+}
