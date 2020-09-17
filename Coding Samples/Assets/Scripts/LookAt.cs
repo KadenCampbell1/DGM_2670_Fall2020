bfe2ccf60c1bf47f8831954e43e0d9464f2a0c47
Kaden(LenLap) <10836246@my.uvu.edu>
2020-09-16 23:59:48 -0600

    LookAt

---
+++ Coding Samples/Assets/Scripts/LookAt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+    public Transform lookedAtObj;
+
+    public void Update()
+    {
+        transform.LookAt(lookedAtObj);
+        var rotationDirection = transform.eulerAngles;
+        rotationDirection.x = 0;
+        transform.rotation = Quaternion.Euler(rotationDirection);
+    }
+}
---
+++ Coding Samples/Assets/Scripts/MouseLocator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLocator : MonoBehaviour
+{
+    private Camera myCamera;
+
+    public Transform pointObj;
+
+    private void Start()
+    {
+        myCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Physics.Raycast(myCamera.ScreenPointToRay(Input.mousePosition), out var hit, 100))
+        {
+            pointObj.position = hit.point;
+        }
+    }
+}
