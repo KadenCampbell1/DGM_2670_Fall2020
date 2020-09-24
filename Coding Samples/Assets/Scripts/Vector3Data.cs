af1cfd45b4dcc8165e2e834d39102a5ad68282ea
Kaden(LenLap) <10836246@my.uvu.edu>
2020-09-23 23:57:08 -0600

    Fixing shooting and cleaning up code

--- Coding Samples/Assets/Scripts/Vector3Data.cs
+++ Coding Samples/Assets/Scripts/Vector3Data.cs
@@ -16,4 +16,12 @@ public class Vector3Data : ScriptableObject
     {
         myValue = objRotation.eulerAngles;
     }
+
+    public void SetValueFromMousePosition(Camera cam)
+    {
+        if (Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out var hit, 100))
+        {
+            myValue = hit.point;
+        }
+    }
 }
