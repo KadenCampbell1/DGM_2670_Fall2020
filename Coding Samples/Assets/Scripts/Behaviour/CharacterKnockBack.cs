b93ead1742191f3ec4c180b0ad715d8d879c839d
Kaden(LenLap) <10836246@my.uvu.edu>
2020-10-01 23:50:44 -0600

    Added KnockBack Script; Worked on particles and lighting

---
+++ Coding Samples/Assets/Scripts/Behaviour/CharacterKnockBack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterKnockBack : MonoBehaviour
+{
+    private CharacterController controller;
+
+    private Vector3 move = Vector3.left;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        controller = GetComponent<CharacterController>();
+        controller.Move(move * Time.deltaTime);
+    }
+
+    private IEnumerator OnTriggerEnter(Collider other)
+    {
+        var i = 2f;
+        move = other.attachedRigidbody.velocity * i;
+        while (i > 0)
+        {
+            yield return new WaitForFixedUpdate();
+            i -= 0.1f;
+        }
+
+        move = Vector3.left;
+    }
+}
