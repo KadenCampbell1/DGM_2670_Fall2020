d60332b1a84a411f7511a9097f8a9b8e538e3a8f
Kaden(LenLap) <10836246@my.uvu.edu>
2020-10-05 23:27:40 -0600

    Created knock back; integrating it into characterBehaviour

--- Coding Samples/Assets/Scripts/Behaviour/CharacterBehaviour.cs
+++ Coding Samples/Assets/Scripts/Behaviour/CharacterBehaviour.cs
@@ -1,14 +1,18 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
 public class CharacterBehaviour : MonoBehaviour
 {
 	private CharacterController myController;
-	private Vector3 v3Movement;
+	private Vector3 v3Movement, knockBackMovement;
 	private float yAxisVar;
 
-	public float dodgeForce = 15f, myRotateSpeed = 150f, myRotateBoost = 100f, myJumpForce = 5f, myGravity = -9.81f;
+	public float dodgeForce = 15f, 
+		myRotateSpeed = 150f, myRotateBoost = 100f,
+		myJumpForce = 5f, myGravity = -9.81f,
+		pushForce = 10f;
 	public bool drivingCar, canDodge;
 
 	public FloatData normalSpeed, fastSpeed, myCarSpeed;
@@ -96,6 +100,30 @@ public class CharacterBehaviour : MonoBehaviour
 		
 		
 		myController.Move(v3Movement * Time.deltaTime);
+	}
+
+	private IEnumerator KnockBack(ControllerColliderHit hit)
+	{
+		var i = 2f;
+		knockBackMovement = hit.collider.attachedRigidbody.velocity * i;
+		while (i > 0)
+		{
+			yield return new WaitForFixedUpdate();
+			i -= 0.1f;
+		}
+	}
+
+	private void OnControllerColliderHit(ControllerColliderHit hit)
+	{
+		var rBody = hit.collider.attachedRigidbody;
+		if (rBody == null || rBody.isKinematic)
+		{
+			return;
+		}
+		
+		StartCoroutine(KnockBack(hit));
+		var pushDirection = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+		rBody.velocity = pushDirection * pushForce;
 	}
 }
 
---
+++ Coding Samples/Assets/Scripts/Behaviour/CharacterKnockBackBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterKnockBackBehaviour : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 move = Vector3.left;
+    public float pushPower = 10f;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        controller.Move(move * Time.deltaTime);
+    }
+
+    private IEnumerator KnockBack(ControllerColliderHit hit)
+    {
+        var i = 2f;
+        move = hit.collider.attachedRigidbody.velocity * i;
+        while (i > 0)
+        {
+            yield return new WaitForFixedUpdate();
+            i -= 0.1f;
+        }
+        move = Vector3.left;
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        var rBody = hit.collider.attachedRigidbody;
+
+        if (rBody == null || rBody.isKinematic)
+        {
+            return;
+        }
+
+        StartCoroutine(KnockBack(hit));
+        var pushDirection = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+        rBody.velocity = pushDirection * pushPower;
+    }
+}
