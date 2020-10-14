d7cdc38cc4a66028c432bc5a5af7e6796349785e
Kaden(LenLap) <10836246@my.uvu.edu>
2020-10-13 23:29:52 -0600

    Finished AI hunt and patrol

--- Coding Samples/Assets/_Scripts/Behaviour/AIBehaviour.cs
+++ Coding Samples/Assets/_Scripts/Behaviour/AIBehaviour.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -10,15 +11,38 @@ public class AIBehaviour : MonoBehaviour
     public Transform player;
     public bool canHunt, canPatrol;
     private WaitForFixedUpdate wffu = new WaitForFixedUpdate();
+    private WaitForSeconds wfs = new WaitForSeconds(2f);
+    public List<Transform> patrolPoints;
+    private int i = 0;
 
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        i = 0;
+        StartCoroutine(Patrol());
     }
 
+    private IEnumerator Patrol()
+    {
+        canPatrol = true;
+        canHunt = false;
+        while (canPatrol)
+        {
+            yield return wffu;
+            if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            {
+                yield return wfs;
+                agent.destination = patrolPoints[i].position;
+                i = (i + 1) % patrolPoints.Count;
+                
+            }
+        }
+    }
+    
     private IEnumerator OnTriggerEnter(Collider other)
     {
         canHunt = true;
+        canPatrol = false;
         while (canHunt)
         {
             yield return wffu;
@@ -26,9 +50,9 @@ public class AIBehaviour : MonoBehaviour
         }
     }
 
-    private void OnTriggerExit(Collider other)
+    private IEnumerator OnTriggerExit(Collider other)
     {
-        canHunt = false;
-        canPatrol = true;
+        yield return wfs;
+        StartCoroutine(Patrol());
     }
 }
