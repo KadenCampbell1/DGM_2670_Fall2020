37761b6b25636844c65eefd61c8d1faf36ff16cc
Kaden(LenLap) <10836246@my.uvu.edu>
2020-10-26 20:46:11 -0600

    Fixed character mover inheritance
    Added character behaviour base to inherit from

--- Coding Samples/Assets/_Scripts/Behaviour/CharacterBehaviour.cs
+++ Coding Samples/Assets/_Scripts/Behaviour/CharacterBehaviour.cs
@@ -2,90 +2,12 @@
 using System.Collections;
 using UnityEngine;
 
-[RequireComponent(typeof(CharacterController))]
-public class CharacterBehaviour : MonoBehaviour
+
+public class CharacterBehaviour : CharacterBehaviourBase
 {
-	private CharacterController myController;
-	private Vector3 v3Movement, knockBackMovement = Vector3.zero, lookDirection;
-	private float yAxisVar;
-
-	public float dodgeForce = 15f, 
-		myRotateSpeed = 150f, myRotateBoost = 100f,
-		myGravity = -9.81f,
-		pushForce = 10f;
-	public bool drivingCar, canDodge;
-
-	public FloatData normalSpeed, fastSpeed, myCarSpeed, myJumpForce;
-	private FloatData currentSpeed;
-
-	public IntData playerJumpMax;
-	private int jumpCount;
-
-	void Start()
-	{
-	    myController = GetComponent<CharacterController>();
-	    currentSpeed = normalSpeed;
-	}
-
-
 	void Update()
 	{
-		yAxisVar += myGravity * Time.deltaTime;
-
-		if (myController.isGrounded && v3Movement.y < 0)
-		{
-			yAxisVar = -1f;
-			jumpCount = 0;
-		}
-
-		if (!drivingCar)
-		{
-			if (Input.GetButtonDown("Fire3"))
-			{
-				currentSpeed = fastSpeed;
-			}
-			else if (Input.GetButtonUp("Fire3"))
-			{
-				currentSpeed = normalSpeed;
-			}
-			
-			if (Input.GetButtonDown("Jump") && jumpCount < playerJumpMax.myValue)
-			{
-				yAxisVar = myJumpForce.value;
-				jumpCount++;
-			}
-
-			if (Input.GetButtonDown("Fire2")) 
-			{ 
-				Debug.Log("Dodge"); 
-				//mySpeed = dodgeForce; 
-				canDodge = false;
-			}
-			
-			if (Input.GetButtonUp("Fire2")) 
-			{ 
-				currentSpeed = normalSpeed; 
-				canDodge = true;
-			}
-			
-			
-			var verticalInput = Input.GetAxis("Vertical") * currentSpeed.value;
-			var horizontalInput = Input.GetAxis("Horizontal") * currentSpeed.value;
-			
-			lookDirection.Set(horizontalInput, 0, verticalInput);
-
-			if (lookDirection == Vector3.zero)
-			{
-				lookDirection.Set(0.0001f, 0, 0.0001f);
-			}
-        
-			if (horizontalInput > 0.5f || horizontalInput < -0.5f ||verticalInput > 0.5f || verticalInput < -0.5f)
-			{
-				transform.rotation = Quaternion.LookRotation(lookDirection);
-			}
-			
-			v3Movement.Set(horizontalInput, yAxisVar, verticalInput);
-		}
+		MoveCharacter();
 		if (drivingCar)
 		{
 			var verticalInput = Input.GetAxis("Vertical") * myCarSpeed.value;
@@ -106,8 +28,7 @@ public class CharacterBehaviour : MonoBehaviour
 			v3Movement = transform.TransformDirection(v3Movement);
 		}
 		
-		
-		myController.Move((v3Movement + knockBackMovement) * Time.deltaTime);
+		//myController.Move((v3Movement + knockBackMovement) * Time.deltaTime);
 	}
 
 	private IEnumerator KnockBack(ControllerColliderHit hit)
---
+++ Coding Samples/Assets/_Scripts/Behaviour/CharacterBehaviourBase.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterBehaviourBase : MonoBehaviour
+{
+    protected CharacterController myController;
+    protected Vector3 v3Movement, knockBackMovement = Vector3.zero, lookDirection;
+    protected float yAxisVar;
+
+    public float dodgeForce = 15f, 
+        myRotateSpeed = 150f, myRotateBoost = 100f,
+        myGravity = -9.81f,
+        pushForce = 10f;
+    public bool drivingCar, canDodge;
+
+    public FloatData normalSpeed, fastSpeed, myCarSpeed, myJumpForce;
+    protected FloatData currentSpeed;
+
+    public IntData playerJumpMax;
+    protected int jumpCount;
+    
+    void Start()
+    {
+        myController = GetComponent<CharacterController>();
+        currentSpeed = normalSpeed;
+    }
+    
+    public void MoveCharacter()
+    	{
+    		yAxisVar += myGravity * Time.deltaTime;
+    
+    		if (myController.isGrounded && v3Movement.y < 0)
+    		{
+    			yAxisVar = -1f;
+    			jumpCount = 0;
+    		}
+    
+    		if (!drivingCar)
+    		{
+    			if (Input.GetButtonDown("Fire3"))
+    			{
+    				currentSpeed = fastSpeed;
+    			}
+    			else if (Input.GetButtonUp("Fire3"))
+    			{
+    				currentSpeed = normalSpeed;
+    			}
+    			
+    			if (Input.GetButtonDown("Jump") && jumpCount < playerJumpMax.myValue)
+    			{
+    				yAxisVar = myJumpForce.value;
+    				jumpCount++;
+    			}
+                
+                var verticalInput = Input.GetAxis("Vertical") * currentSpeed.value;
+    			var horizontalInput = Input.GetAxis("Horizontal") * currentSpeed.value;
+    			
+    			lookDirection.Set(horizontalInput, 0, verticalInput);
+    
+    			if (lookDirection == Vector3.zero)
+    			{
+    				lookDirection.Set(0.0001f, 0, 0.0001f);
+    			}
+            
+    			if (horizontalInput > 0.5f || horizontalInput < -0.5f ||verticalInput > 0.5f || verticalInput < -0.5f)
+    			{
+    				transform.rotation = Quaternion.LookRotation(lookDirection);
+    			}
+    			
+    			v3Movement.Set(horizontalInput, yAxisVar, verticalInput);
+    		}
+
+            myController.Move((v3Movement) * Time.deltaTime);
+    	}
+}
