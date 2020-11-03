24be01dc145945165134184b8fcbd1f06f328506
Kaden(LenLap) <10836246@my.uvu.edu>
2020-11-03 00:04:42 -0700

    Worked on Unity Actions in Healthbar scene

---
+++ Coding Samples/Assets/_Scripts/ScriptableObjectsScripts/IntData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class IntData : ScriptableObject
+{
+    public int myValue;
+
+    public void IncrementFromCustomValue(int incrementValue)
+    {
+        myValue += incrementValue;
+    }
+
+    public void IncrementFromScriptableObject(IntData data)
+    {
+        myValue += data.myValue;
+    }
+}
---
+++ Coding Samples/Assets/_Scripts/ScriptableObjectsScripts/Vector3Data.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Vector3Data : ScriptableObject
+{
+    public Vector3 myValue;
+
+    public void SetValueFromVector3(Vector3 objTransform)
+    {
+        myValue = objTransform;
+    }
+
+    public void SetValueFromPosition(Transform objPosition)
+    {
+        myValue = objPosition.position;
+    }
+    
+    public void SetPositionFromValue(Transform objPosition)
+    {
+        objPosition.position = myValue;
+    }
+
+    public void SetValueFromRotation(Transform objRotation)
+    {
+        myValue = objRotation.eulerAngles;
+    }
+
+    public void SetValueFromMousePosition(Camera cam)
+    {
+        if (Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out var hit, 100))
+        {
+            myValue = hit.point;
+        }
+    }
+}
