9362093fe75ce28b2f9c95dd00b92b9e30b83b65
Kaden(LenLap) <10836246@my.uvu.edu>
2020-11-04 22:05:05 -0700

    Added Animation State Machine Scene
    Added rig and began switching states with script

---
+++ Coding Samples/Assets/_Scripts/Behaviour/PigAnimStateBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PigAnimStateBehaviour : MonoBehaviour
+{
+
+    public UnityEvent forwardEvent, backwardEvent, idleEvent;
+
+    private void Start()
+    {
+        idleEvent.Invoke();
+    }
+
+    private void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") == 1)
+        {
+            forwardEvent.Invoke();
+        }
+
+        if (Input.GetAxisRaw("Horizontal") == -1)
+        {
+            backwardEvent.Invoke();
+        }
+
+        if (!Input.GetButtonDown("Horizontal") && Input.GetAxisRaw("Horizontal") == 0)
+        {
+            idleEvent.Invoke();
+        }
+    }
+}
