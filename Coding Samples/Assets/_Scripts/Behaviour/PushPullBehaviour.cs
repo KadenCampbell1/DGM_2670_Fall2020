c614719b8ef59b2898784566232fc16566e25c13
Kaden(LenLap) <10836246@my.uvu.edu>
2020-11-16 22:35:38 -0700

    Set up Push and pull; Worked on AI brains

---
+++ Coding Samples/Assets/_Scripts/Behaviour/PushPullBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PushPullBehaviour : MonoBehaviour
+{
+    private Rigidbody rBody;
+    public bool canPickUp;
+
+    private void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        canPickUp = Input.GetKey(KeyCode.LeftControl);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (canPickUp)
+        {
+            transform.parent = other.transform;
+            rBody.Sleep();
+        }
+        else
+        {
+            transform.parent = null;
+            rBody.WakeUp();
+        }
+    }
+}
---
+++ Coding Samples/Assets/_Scripts/ScriptableObjectsScripts/AIBrainData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[CreateAssetMenu]
+public class AIBrainData : ScriptableObject
+{
+    public float health, speed;
+    public List<Vector3Data> points;
+    public Transform transform;
+    public MeshFilter mesh;
+
+
+    public virtual void Navigate(NavMeshAgent agent)
+    {
+        agent.destination = transform.position;
+    }
+}
