c8cc6590ede179eace5d841fd45f1496945a213a
Kaden(LenLap) <10836246@my.uvu.edu>
2020-11-18 23:46:44 -0700

    Worked on Instancing AI in RevisedAIScene

--- Coding Samples/Assets/_Scripts/Behaviour/InstancerBehaviour.cs
+++ Coding Samples/Assets/_Scripts/Behaviour/InstancerBehaviour.cs
@@ -2,11 +2,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class InstancerBehaviour : MonoBehaviour
 {
     public GameObject prefab;
     public Vector3Data rotationDirection;
+    public float holdTime = 0.5f;
+    public bool canLoop = false;
+    public int instanceCount = 30;
+    private int counter = 0;
+    
+    public UnityEvent startEvent, onCallEvent;
+    
+    private WaitForSeconds wfs;
+
+    private void Start()
+    {
+        wfs = new WaitForSeconds(holdTime);
+        startEvent.Invoke();
+    }
+
+    public void StartLoopEvents()
+    {
+        canLoop = true;
+        StartCoroutine(CallInstanceEvent());
+    }
+    
+    private IEnumerator CallInstanceEvent()
+    {
+        while (canLoop && counter < instanceCount)
+        {
+            onCallEvent.Invoke();
+            counter++;
+            yield return wfs;
+        }
+        canLoop = false;
+        counter = 0;
+    }
 
     public void InstanceWithRotationDirection()
     {
